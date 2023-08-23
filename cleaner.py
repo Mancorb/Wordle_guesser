45e9825ea1279536b383c65582b49bd32e2f7ce9
Mancorb <andres.m.bosques@gmail.com>
2023-08-23 12:05:22 -0600

    New spanish wordlist
    small improvements to the cleaner for particular cases of the wordlist

--- cleaner.py
+++ cleaner.py
@@ -1,20 +1,24 @@
 import re
-import os
+
 words = []
+loc="new.txt"
 
-
-with open("wordlist_esp.txt", "r") as file:
+with open(loc, "r", encoding="utf8") as file: # utf8 for special characters
     for line in file:
         if line != "\n":
-            res = re.findall("[^0-9,.\n]",line)
+            res = re.findall("[^0-9,.\n]",line)# Delete all numbers and spaces
             word=""
-            for i in res:
-                word=word+i
+            for i in res: #join all found matches
+                if i =="/": # sparate cases like "lustro / dias" to get just one and continue withouth adding the "/"
+                    words.append(word.strip())
+                    word=""
+                else:
+                    word=word+i
             words.append(word.strip())
 
-print("+\tData extracted")
+print("[+] Data extracted")
 
-with open("wordlist_esp_.txt","w") as file:
+with open("wordlist_esp.txt","a", encoding="utf8") as file:
     for i in words:
-        file.write(i+"\n")
-print("+\tProcess finnished")
+        file.write("\n"+i)
+print("[+] Process finnished")
